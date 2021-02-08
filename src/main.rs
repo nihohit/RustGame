5639718d58e750d916eebcbc6caf0df41f351c3c
Shachar <nihohit@gmail.com>
2021-02-08 20:57:51 +0200

    render text in window.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use bevy::prelude::*;
+
+/// This example illustrates how to create text and update it in a system. It displays the current FPS in the upper left hand corner.
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_resource(WindowDescriptor {
+            title: "I am a window!".to_string(),
+            width: 500.,
+            height: 300.,
+            vsync: true,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
+    commands
+        // 2d camera
+        .spawn(CameraUiBundle::default())
+        .spawn(TextBundle {
+            style: Style {
+                align_self: AlignSelf::Center,
+                position_type: PositionType::Absolute,
+                position: Rect {
+                    bottom: Val::Px(5.0),
+                    left: Val::Px(15.0),
+                    right: Val::Px(15.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            text: Text {
+                value: "This is where the text appears".to_string(),
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                style: TextStyle {
+                    font_size: 30.0,
+                    color: Color::WHITE,
+                    alignment: TextAlignment {
+                        vertical: VerticalAlign::Bottom,
+                        horizontal: HorizontalAlign::Center,
+                    },
+                    ..Default::default()
+                },
+            },
+            ..Default::default()
+        });
 }
