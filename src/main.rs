944658adbd54fcda1cf2ed7e8a84b1abcaa2761b
Shachar <nihohit@gmail.com>
2021-02-15 22:39:26 +0200

    Render all the pieces and player according to their location.

--- src/main.rs
+++ src/main.rs
@@ -23,89 +23,115 @@ const TILE_SIZE: f32 = 100.0;
 
 fn setup_ui(commands: &mut Commands, asset_server: Res<AssetServer>) {
     commands
-    // 2d camera
-    .spawn(CameraUiBundle::default())
-    .spawn((InputText,))
-    .with_bundle(TextBundle {
-        style: Style {
-            align_self: AlignSelf::Center,
-            position_type: PositionType::Absolute,
-            position: Rect {
-                bottom: Val::Px(5.0),
-                left: Val::Px(15.0),
-                right: Val::Px(15.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-        text: Text {
-            value: "This is where the text appears".to_string(),
-            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-            style: TextStyle {
-                font_size: 30.0,
-                color: Color::WHITE,
-                alignment: TextAlignment {
-                    vertical: VerticalAlign::Bottom,
-                    horizontal: HorizontalAlign::Center,
+        // 2d camera
+        .spawn(CameraUiBundle::default())
+        .spawn((InputText,))
+        .with_bundle(TextBundle {
+            style: Style {
+                align_self: AlignSelf::Center,
+                position_type: PositionType::Absolute,
+                position: Rect {
+                    bottom: Val::Px(5.0),
+                    left: Val::Px(15.0),
+                    right: Val::Px(15.0),
+                    ..Default::default()
                 },
                 ..Default::default()
             },
-        },
-        ..Default::default()
-    });
-}
-
-fn setup_board(commands: &mut Commands, asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>,) {
-    let black_tile = asset_server.load("textures/black_tile.png");
-    let white_tile = asset_server.load("textures/white_tile.png");
-    let player = asset_server.load("textures/player.png");
-
-    let board = model::create_board();
-
-    commands.spawn(Camera2dBundle::default());
-
-    let middle_row_index:i16 = (board.tiles.len() / 2) as i16;
-    for row_index in 0..board.tiles.len() as i16 {
-        let row = &board.tiles[row_index as usize];
-        let middle_index:i16 = (row.len() / 2) as i16;
-        for tile_index in 0..row.len() as i16 {
-            let tile = &row[tile_index as usize];
-            let transform = Transform {
-                translation: Vec3 {
-                    x: f32::from(tile_index - middle_index) * TILE_SIZE ,
-                    y: f32::from(row_index - middle_row_index) * TILE_SIZE,
-                    z:0.0
+            text: Text {
+                value: "This is where the text appears".to_string(),
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                style: TextStyle {
+                    font_size: 30.0,
+                    color: Color::WHITE,
+                    alignment: TextAlignment {
+                        vertical: VerticalAlign::Bottom,
+                        horizontal: HorizontalAlign::Center,
+                    },
+                    ..Default::default()
                 },
-                ..Default::default()
-            };
-            match tile.color {
-                model::TileColor::Black => {commands.spawn(Camera2dBundle::default())
-                .spawn(SpriteBundle {
-                    material: materials.add(black_tile.clone().into()),
-                    transform: transform,
-                    ..Default::default()
-                });
             },
-            
-            model::TileColor::White => {commands.spawn(Camera2dBundle::default())
-                .spawn(SpriteBundle {
-                    material: materials.add(white_tile.clone().into()),
-                    transform: transform,
-                    ..Default::default()
-                });
-            }
-        }
-        }
-    }
-
-    commands.spawn(SpriteBundle {
-            material: materials.add(player.into()),
             ..Default::default()
         });
 }
 
-fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>, mut text_query: Query<Mut<Text>, With<InputText>>) {
+fn coords_to_transform(x: i16, y: i16) -> Transform {
+    return Transform {
+        translation: Vec3 {
+            x: f32::from(x) * TILE_SIZE,
+            y: f32::from(y) * TILE_SIZE,
+            z: 0.0,
+        },
+        ..Default::default()
+    };
+}
+
+fn setup_board(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let black_tile = asset_server.load("textures/black_tile.png");
+    let white_tile = asset_server.load("textures/white_tile.png");
+    let white_piece = asset_server.load("textures/WhitePiece.png");
+    let black_piece = asset_server.load("textures/BlackPiece.png");
+
+    let render_tile_material = |entity_type: &model::TileColor| match entity_type {
+        model::TileColor::White => white_tile.clone().into(),
+        model::TileColor::Black => black_tile.clone().into(),
+    };
+
+    let render_entity_material = |entity_type: &model::EntityType| match entity_type {
+        model::EntityType::WhitePiece => white_piece.clone().into(),
+        model::EntityType::BlackPiece => black_piece.clone().into(),
+    };
+    let board = model::create_board();
+
+    commands.spawn(Camera2dBundle::default());
+
+    let middle_row_index: i16 = (board.tiles.len() / 2) as i16;
+    for row_index in 0..board.tiles.len() as i16 {
+        let row = &board.tiles[row_index as usize];
+        let middle_index: i16 = (row.len() / 2) as i16;
+        for tile_index in 0..row.len() as i16 {
+            let tile = &row[tile_index as usize];
+            let transform =
+                coords_to_transform(tile_index - middle_index, row_index - middle_row_index);
+            commands.spawn(SpriteBundle {
+                material: materials.add(render_tile_material(&tile.color)),
+                transform: transform,
+                ..Default::default()
+            });
+
+            match &tile.entity {
+                None => {}
+                Some(entity_type) => {
+                    commands.spawn(SpriteBundle {
+                        material: materials.add(render_entity_material(entity_type)),
+                        transform: transform,
+                        ..Default::default()
+                    });
+                }
+            }
+        }
+    }
+
+    let row = board.tiles[board.player.x as usize];
+    let middle_index: i16 = (row.len() / 2) as i16;
+    commands.spawn(SpriteBundle {
+        material: materials.add(render_entity_material(&board.player.entity_type)),
+        transform: coords_to_transform(
+            board.player.x - middle_index,
+            board.player.y - middle_row_index,
+        ),
+        ..Default::default()
+    });
+}
+
+fn keyboard_input_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut text_query: Query<Mut<Text>, With<InputText>>,
+) {
     if keyboard_input.pressed(KeyCode::Up) {
         for mut text in text_query.iter_mut() {
             text.value = "Pressing up".to_string();
