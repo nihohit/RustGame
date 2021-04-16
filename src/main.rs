bcf8b621213be0ae424a2b118bbf7e8588447dd4
Shachar <nihohit@gmail.com>
2021-04-16 11:31:41 +0200

    temp

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use bevy::math::vec2;
 use bevy::prelude::*;
 use rand::prelude::*;
 
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemLabel)]
+struct Calculations;
+
 /// This example illustrates how to create text and update it in a system. It displays the current FPS in the upper left hand corner.
 fn main() {
     App::build()
@@ -14,18 +17,18 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup_boids.system())
-        .add_system(coherence.system())
-        .add_system(separation.system())
-        .add_system(alignment.system())
-        .add_system(final_update.system())
+        .add_system(coherence.system().label(Calculations))
+        // .add_system(separation.system().label(Calculations))
+        // .add_system(alignment.system().label(Calculations))
+        .add_system(final_update.system().after(Calculations))
         .run();
 }
 
 struct Boid {
-    next_x: f32,
-    next_y: f32,
+    coherence_result: Vec2,
+    separation_result: Vec2,
+    alignemnt_result: Vec2,
     velocity: Vec2,
-    next_velocity: Vec2,
 }
 
 fn setup_boids(
@@ -33,7 +36,7 @@ fn setup_boids(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let boid = asset_server.load("textures/boid.png");
+    let boid = asset_server.load("textures/black_tile.png");
     const BOID_COUNT: i16 = 200;
 
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
@@ -52,10 +55,31 @@ fn setup_boids(
                 ..Default::default()
             })
             .insert(Boid {
-                next_x: transform.translation.x,
-                next_y: transform.translation.y,
+                coherence_result: Vec2::ZERO,
+                separation_result: Vec2::ZERO,
+                alignemnt_result: velocity,
                 velocity: velocity,
-                next_velocity: velocity,
             });
     }
 }
+
+fn coherence(
+    mut boids: Query<(Entity, &Transform, &mut Boid)>,
+    otherBoids: Query<(Entity, &Transform)>,
+) {
+    const COHERENCE_DISTANCE: f32 = 20;
+    for (entity, transform, boid) in boids.iter_mut() {
+        let mut coherence = Vec3::ZERO;
+        let mut count = 0;
+        for (otherEntity, otherTransform) in otherBoids.iter() {
+            if otherEntity != entity
+                && transform.translation.distance(otherTransform.translation) < COHERENCE_DISTANCE
+            {
+                coherence += otherTransform.translation;
+                count += 1;
+            }
+        }
+    }
+}
+
+fn final_update(boids: Query<(Entity, &Transform, &mut Boid)>) {}
